8ba89557b97c72d39103165a5555609ca310f81c
betolix <betolix@gmail.com>
2023-03-24 23:10:42 +0000

    Wohoooo

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,18 +1,9 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: null,
+			planets: null,
+			vehicles: null,
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -37,8 +28,45 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+
+			getCharacter: async () => {
+				try{
+					const response = await fetch("https://swapi.dev/api/people");
+					const data = await response.json();
+					setStore({ characters: data.results});
+					console.log(data.results);
+					return data.results;
+				}
+				catch(error){
+					//console.log(error);
+				}
+			},
+
+			getPlanet: async () => {
+				try{
+					const response = await fetch("https://swapi.dev/api/planets");
+					const data = await response.json();
+					setStore({ planets: data.results});
+					return data.results;
+				}
+				catch(error){
+					//console.log(error);
+				}
+			},
+
+			getVehicle: async () => {
+				try{
+					const response = await fetch("https://swapi.dev/api/starships");
+					const data = await response.json();
+					setStore({ vehicles: data.results});
+					return data.results;
+				}
+				catch(error){
+					//console.log(error);
+				}
 			}
-		}
+		},
 	};
 };
 
